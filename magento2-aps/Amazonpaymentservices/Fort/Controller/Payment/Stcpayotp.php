f516fd99d8d4d3501070582beec1367907d8d24a
Pragadheeswaran P <ppragad@amazon.com>
2026-09-07 10:40:37 +0530

    Fix: centralize debug-log redaction in Helper\Data::log() to prevent sensitive payment data leaks

--- magento2-aps/Amazonpaymentservices/Fort/Controller/Payment/Stcpayotp.php
+++ magento2-aps/Amazonpaymentservices/Fort/Controller/Payment/Stcpayotp.php
@@ -56,7 +56,7 @@ class Stcpayotp extends \Magento\Framework\App\Action\Action implements CsrfAwar
     {
         
         $quote = $this->_cart->getQuote();
-        $this->_helper->log(json_encode($this->_helper->sanitizeForLog($quote->getData())));
+        $this->_helper->log(json_encode($this->_helper->sanitizeOrderDataForLog($quote->getData())));
         $quote->reserveOrderId()->save();
         $orderId = $quote->getReservedOrderId();
 
